f8c1626a37a9d69fe8a73d8df6b1792a4b2e18cf
Derpy-Jacob-903 <73241159+Derpy-Jacob-903@users.noreply.github.com>
2023-06-05 00:38:11 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,11 +3,11 @@ namespace SallyPokehellHero;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "36.1";
-    public const string Version = "1.0.0";
+    public const string Version = "0.3.1";
     public const string Name = "SallyPokehellHero";
 
-    public const string Description = "An empty mod";
+    public const string Description = "Add Sally from Pokehell to Bloons TD 6 as new Hero!";
 
-    public const string RepoOwner = ""; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoOwner = "Derpy-Jacob-903"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "SallyPokehellHero"; // TODO add your repo name here, also in the download url in README.md
 }
